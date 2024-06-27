e330f53ded018203a5d1205b34072eb3b17b1101
hanni-hi <bittersweeety@gmail.com>
2024-06-27 13:58:14 +0900

    [upload] 교수님 자료+6.27일 이전 조별발표자료

---
+++ 20240516_C# 교수님예시_객체생성_클래스와 구조체/20240516/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20240516
+{
+    enum Gender
+    {
+        Woman=0,
+        Man
+    }
+    class player
+    {
+        //oop -> 객체지향의 특징 
+        //캡슐화 은닉화 
+        public string name { get; private set; }
+        public Gender gender { get; private set; }
+        public void Setting_name(string _name)
+        {
+            name = _name;
+            Console.WriteLine("이름 설정 완료!");
+        }
+        public void Setting_gender(int i)
+        {
+            if(i==0)
+            {
+                gender = Gender.Woman;
+            }
+            else
+            {
+                gender = Gender.Man;
+
+            }
+        }
+    }
+
+    struct Player_s
+    {
+       public string name;
+    }
+
+    class Program
+    {
+        int a;//Program a라는 변수
+        public static int b;//글로벌 변수
+        // static void Main(string[] args)
+        // 전역에서 활동하는 친구 -> 전역에서 활동하는 메서드 
+        // 전역변수는 그냥 참조 가능 / 객체(class) 멤버변수는 접근할 떄 객체를 생성
+        //void Main(string[] args)
+        //-> Program 이라는 클래스의 메서드
+        static void Main(string[] args)//전역
+        {
+            b = 0;//글로벌 변수 -> static main 접근 가능.
+
+            //Program p = new Program();
+            //p.a = 0;
+            #region
+            /*
+            // c++                    c#
+            //    입력 -> cin      -> console.readline 
+            //    출력 -> cout  -> console.write(endl X) writeline(endl o)
+             
+            Console.WriteLine("안녕하세요. 월드입니다.");
+            Console.Write("이름을 입력하세용 : ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            */
+
+            //조건문 if
+            //int a = 7;
+            //if (a < 7)
+            //{
+            //    Console.WriteLine("a는 7보다 작다");
+            //}
+            //else if (a < 5)
+            //{
+            //    Console.WriteLine("a는 5보다 작다");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a는 뭔지 모르겟다");
+            //}
+            #endregion
+            #region
+            //for
+            //for(int i=0;i<10;i++)
+            //{
+            //    Console.WriteLine("i는 "+i);
+            //}
+
+            //배열선언
+            //c++
+            //int array[배열크기];
+            //c#
+            /*
+            int[] array = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                array[i] = i;//  기본 자료형 -> 객체? 자료형  
+                Console.WriteLine(" array[i]는 " + array[i]);
+            }
+
+
+            player[] players = new player[10];//배열을 생성
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i] = new player();//객체이기 떄문에 생성자 키워드를 사용
+                players[i].name = Console.ReadLine();
+                Console.WriteLine("  players[i].name 은" + players[i].name);
+            }
+            */
+            /*
+
+            int case_index = 0;
+            while (true)
+            {
+
+                case_index = int.Parse(Console.ReadLine());
+
+                switch (case_index)
+                {
+                    case 0:
+                        Console.WriteLine("안녕 나는 정해창~");
+                        break;
+                    case 1:
+                        Console.WriteLine("나는 먹은것 따윈 정리하지 않지");
+                        break;
+                    case 2:
+                        Console.WriteLine("그래서 성진이가 해줬어");
+                        break;
+                    case 3:
+                        Console.WriteLine("나는 성진이한테 커피를 사줬어");
+                        break;
+                    case 4:
+                        Console.WriteLine("나도 치웠는데...나는 나쁜놈이...");
+                        break;
+                    default:
+                        Console.WriteLine("전 이런 놈입니다.");
+                        break;
+                }
+            }
+            */
+            #endregion
+            #region
+            //player player_1 = new player();//참조
+            //Player_s player_s;//Value
+            //
+            ////int a-> 값
+            //
+            //player_1.name = "정해창";
+            //player_s.name = "김혜린";
+            //
+            //Console.WriteLine(player_1.name);
+            //Console.WriteLine(player_s.name);
+            #endregion
+            #region
+            //int[] array = new int[5];
+            //for(int i=0;i<array.Length;i++)
+            //{
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //
+            //Print_array(array);
+            #endregion
+            player _player = new player();
+            Console.Write("닉네임을 입력해 주세요 : ");
+            //Setting_name
+            _player.Setting_name(Console.ReadLine());
+            _player.Setting_gender(1);
+            Console.WriteLine(_player.gender + " 님 환영합니다!!!");
+            Console.WriteLine(_player.name + " 님 환영합니다!!!");
+            Console.Write("닉네임을 변경해 주세요 : ");
+            _player.Setting_name(Console.ReadLine());
+            Console.WriteLine(_player.name + " 님 환영합니다!!!");
+
+        }
+
+        //매개변수 배열을 넣으면 그 안에 있는 배열의 인자를 출력하는 메서드 -> 전역
+        public static void Print_array(int[] array)
+        {
+
+            for(int i=0;i<array.Length;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+
+    }
+}
+
---
+++ 교수님이만드신거_c++/교수님이만드신거/교수님이만드신거.cpp
@@ -0,0 +1,318 @@
+﻿// 교수님이만드신거.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+​#include <iostream>
+#include "NumberGame.h"
+
+using namespace std;
+enum Operater
+{
+    Sum = 0,      //더하기
+    Subtract,   //뺴기
+    Multip,     //곱하기
+    Division    //나누기
+};
+
+class Num_card//숫자카드
+{
+public:
+    int index;
+    bool isUsing;
+};
+
+class Oper_card
+{
+public:
+    Operater oper;
+    bool isUsing;
+};
+
+//메소드 분리
+//카드뽑기  v
+//Enum->string v
+//배팅 v
+//결과도출
+
+//숫자카드뽑기-배열
+int Drawing_numcard(Num_card* numcardarray)
+{
+    while (true)
+    {
+        int index = rand() % 10;
+        if (numcardarray[index].isUsing)
+        {
+            continue;
+        }
+        else
+        {
+            numcardarray[index].isUsing = true;
+            return index;
+        }
+    }
+}
+
+
+int Drawing_Oper(Oper_card* oper)
+{
+    while (true)
+    {
+        int index = rand() % 4;
+        if (oper[index].isUsing)
+        {
+            continue;
+        }
+        else
+        {
+            oper[index].isUsing = true;
+            return index;
+        }
+    }
+}
+//Enum,-> string
+string Oper_toString(Operater oper)
+{
+    switch (oper)
+    {
+    case Sum:
+        return "+";
+    case Subtract:
+        return "-";
+    case Multip:
+        return "X";
+    case Division:
+        return "/";
+    default:
+        return "";
+    }
+}
+//베팅 -> 검사 10% 이상 걸엇냐?
+bool betting_check(int betting_money, int player_currentMoney)
+{
+    if (betting_money < (player_currentMoney * 0.1))
+    {
+        cout << "너무적어 다시해 손모가지 나가고 싶어?" << endl;
+        return false;
+    }
+    else if (player_currentMoney <= 0)
+    {
+        cout << "그지새끼니 넘어갑니다..." << endl;
+        return false;
+    }
+    return true;
+}
+//결과도출
+//숫자카드 2장 / 연산카드 1장 
+int Result_count(Num_card* numcard, Oper_card oper, string playername)
+{
+    int frist_card = numcard[0].index;
+    int se_card = numcard[1].index;
+
+    int result = 0;//반환할 값
+    switch (oper.oper)
+    {
+    case Sum:
+        result = frist_card + se_card;
+        break;
+    case Subtract:
+        result = frist_card - se_card;
+        break;
+    case Multip:
+        result = frist_card * se_card;
+        break;
+    case Division:
+        result = frist_card / se_card;
+        break;
+    }
+
+    cout << endl;
+    cout << playername << "의 값은" << frist_card << Oper_toString(oper.oper)
+        << se_card << " = " << result << endl;
+    cout << endl;
+
+    return result;
+
+
+}
+//승이나 패냐 승패를 나누는 결과
+bool Result(Num_card* player_card, Num_card* com_card,
+    Oper_card Player_oper, Oper_card com_oper)
+{
+    int player_result = Result_count(player_card, Player_oper, "[Player]");
+    int com_result = Result_count(com_card, com_oper, "[Com]");
+
+    /*
+    승리조건
+      1. 7이면 무조건 승리
+      2. 높은 숫자이면 높은 수를 가진 사람이 승리
+
+    */
+    if (player_result == 7 || com_result == 7)
+    {
+        if (player_result == 7)
+        {
+            return true;//이 메소드 플레이어기준
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        if (player_result > com_result)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+    Num_card numcard[10];//전체가 사용할 카드
+    Num_card Player_num[2];//Player 습득한 카드
+    Num_card Com_num[2];//com 습득한 카드
+
+    Oper_card operCard[4];
+    Oper_card Player_operCard;
+    Oper_card Com_operCard;
+
+    //초기 배팅 금액 
+    int Player_money = 10000;
+    int Com_money = 10000;
+
+    //
+    int All_betting_money = 0;//딜러~
+    int Round_count = 0;//전체 라운드 카운트 
+    while (true)
+    {
+        //카드를 초기화 -> 새로운 게임의 대한 초기화
+        if (Round_count == 0)
+        {
+            cout << "--------------------------------------------" << endl;
+            //num카드 초기화
+            for (int i = 0; i < 10; i++)
+            {
+                numcard[i].index = i + 1;
+                numcard[i].isUsing = false;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                //열거형 -> index
+                operCard[i].oper = (Operater)i;
+                operCard[i].isUsing = false;
+            }
+            All_betting_money = 0;
+        }
+
+        //라운드가 진행될때마다 카드를 분배
+        if (Round_count >= 0)
+        {
+            //카드를 배열의 index
+            int player_index = 0;
+            int com_index = 0;
+            //순서의 따른 카드 배분
+            switch (Round_count)
+            {
+            case 0://첫번쨰 카드 뽑기
+                cout << "첫번쨰 카드를 배분합니다....." << endl;
+                player_index = Drawing_numcard(numcard);
+                com_index = Drawing_numcard(numcard);
+                Player_num[0] = numcard[player_index];
+                Com_num[0] = numcard[com_index];
+                cout << "Player가 뽑은 카드 :  " << Player_num[0].index << endl;
+                break;
+            case 1:
+                //연산카드 배분
+                cout << "연산카드를 배분합니다..." << endl;
+                player_index = Drawing_Oper(operCard);
+                com_index = Drawing_Oper(operCard);
+                Player_operCard = operCard[player_index];
+                Com_operCard = operCard[com_index];
+                cout << "Player가 뽑은 연산카드 :" << Oper_toString(Player_operCard.oper) << endl;
+                break;
+            case 2:
+                cout << "두번쨰 카드를 배분합니다....." << endl;
+                player_index = Drawing_numcard(numcard);
+                com_index = Drawing_numcard(numcard);
+                Player_num[1] = numcard[player_index];
+                Com_num[1] = numcard[com_index];
+                cout << "Player가 뽑은 카드 :  " << Player_num[1].index << endl;
+                break;
+            case 3:
+                //배팅만
+                cout << "마지막 배팅을 해주세용" << endl;
+                break;
+            }
+
+            //배팅작업
+            int round_Betting_money = 0;
+            while (true)
+            {
+                cout << "베팅할 금액을 입력해주세요(현재금액 : " << Player_money << ") : " << endl;
+                cin >> round_Betting_money;
+                if (betting_check(round_Betting_money, Player_money))
+                {
+                    if (Player_money > 0)
+                    {
+                        Player_money -= round_Betting_money;
+                        Com_money -= round_Betting_money;
+                        All_betting_money += (round_Betting_money * 2);//딜러니까
+
+                    }
+                    Round_count += 1;
+                    break;
+                }
+            }
+
+            //결과
+            if (Round_count == 3)
+            {
+                //결과노출
+                if (Result(Player_num, Com_num, Player_operCard, Com_operCard))
+                {
+                    //승리
+                    cout << "[Player]가 승리하였습니다" << endl;
+                    cout << All_betting_money << "원의 금액을 얻었습니다." << endl;
+                    Player_money += All_betting_money;
+                }
+                else
+                {
+                    //패배
+                    cout << "[Computer]가 승리하였습니다" << endl;
+                    cout << All_betting_money << "원의 금액을 잃었습니다." << endl;
+                    Com_money += All_betting_money;
+                }
+                Round_count = 0;
+            }
+
+            //게임종료조건
+            /*
+                    둘중에 한명이 모든돈을 잃었다면 게임이 종료가 된다
+                    하지만 게임이 진행되고 있다면? 종료하면 안된다.
+            */
+            //게임종료 예외처리
+            if (Round_count <= 0 && (Player_money <= 0 || Com_money <= 0))
+            {
+                if (Player_money <= 0)
+                {
+                    cout << "정말 그지가 되셨군요...꺼지세요...ㅃㅃ" << endl;
+                }
+                else
+                {
+                    cout << "컴퓨터를 파산내셨습니다....하하 ㅊㅊ" << endl;
+                }
+                break;
+            }
+        }
+    }
+    cout << "게임 종료" << endl;
+
+}
